6fc4d5d69a261d151bb82c6f49acfd33c13b7ff0
prakarshhh <prakarshkashyap10@gmail.com>
2023-08-26 11:51:06 +0530

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,47 @@
+const socket = io();
+
+let senderName = prompt("Please enter your name:"); // Prompt for the sender's name
+
+// Validate the sender's name (you can add more validation as needed)
+while (!senderName || senderName.trim() === "") {
+  senderName = prompt("Name cannot be empty. Please enter your name:");
+}
+
+// Display the sender's name in the chat header
+const chatHeader = document.querySelector('.chat-header');
+chatHeader.textContent = `WhatsApp Clone - ${senderName}`;
+
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const chatMessages = document.querySelector('.chat-messages');
+
+// Event listener for the send button
+sendButton.addEventListener('click', () => {
+  const message = messageInput.value;
+  if (message.trim() !== '') {
+    socket.emit('chat message', { sender: senderName, message });
+    messageInput.value = '';
+  }
+});
+
+// Listen for incoming messages
+socket.on('chat message', (data) => {
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message');
+
+  // Create a div for the sender's name
+  const senderNameElement = document.createElement('div');
+  senderNameElement.classList.add('sender-name');
+  senderNameElement.textContent = data.sender;
+
+  // Create a div for the message text
+  const messageTextElement = document.createElement('div');
+  messageTextElement.classList.add('message-text');
+  messageTextElement.textContent = data.message;
+
+  // Append sender's name and message text to the message container
+  messageElement.appendChild(senderNameElement);
+  messageElement.appendChild(messageTextElement);
+
+  chatMessages.appendChild(messageElement);
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files (HTML, CSS, JS) from a 'public' directory
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    // Handle new connections here
+    console.log('A user connected');
+
+    // Handle disconnections
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+    // Handle incoming chat messages
+    socket.on('chat message', (message) => {
+        // Broadcast the message to all connected clients
+        io.emit('chat message', message);
+    });
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
